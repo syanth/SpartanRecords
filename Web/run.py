f0b8eccf767e4a12ca41d83a8fd673193afeed59
syanth <syanth@gmail.com>
2018-09-19 21:29:51 -0400

    Fix database path

--- Web/run.py
+++ Web/run.py
@@ -3,17 +3,22 @@
 from flask import Flask, request, render_template #import main Flask class and request object
 import sqlite3
 import json
+from os import path
+
+ROOT = path.dirname(path.realpath(__file__))
+
 
 app = Flask(__name__) #create the Flask app
 
 @app.route("/")
 def hello():
-    conn = sqlite3.connect('games.db')
+    #conn = sqlite3.connect('games.db')
+    conn = sqlite3.connect(path.join(ROOT, "games.db"))
     query = "SELECT * from gamesTBL"
     cur = conn.cursor()
     cur.execute(query)
     data = cur.fetchmany(10)
-    cur.close()
+    conn.close()
     return render_template("template.html", data=data)
 
     #return render_template('template.html', user=user_details)
@@ -36,8 +41,9 @@ def json_example():
         deaths = req_data['deaths']
         assists = req_data['assists']
 
-    conn = sqlite3.connect('games.db')
-    conn.execute("INSERT INTO gamesTBL (name, kills, deaths, assists) VALUES (?, ?, ?, ?)", (name, kills, deaths, assists))
+    conn = sqlite3.connect(path.join(ROOT, "games.db"))
+    cur = conn.cursor()
+    cur.execute("INSERT INTO gamesTBL (name, kills, deaths, assists) VALUES (?, ?, ?, ?)", (name, kills, deaths, assists))
     conn.commit()
     conn.close()
     return '''
@@ -46,4 +52,5 @@ def json_example():
            Assists: {}'''.format(kills, deaths, assists)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) #run app in debug mode on port 5000
+    app.run()
+    #app.run(debug=True, port=5000) #run app in debug mode on port 5000
